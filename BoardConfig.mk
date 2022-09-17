c6404bb7cde4db9510bf6baceafd55d79fddfb69
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-17 18:39:53 +0100

    selene: Define DT2W node for power HAL
    Change-Id: I1dc2f23e0c7764a8ca9d5bf6c9f2f78dcd89dbbe
    Signed-off-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,7 @@ TARGET_BOARD_PLATFORM := mt6768
 
 # Power
 TARGET_POWERHAL_AIDL_VERSION := 1
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
