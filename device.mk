fff3826f3cc33962c82057b6923def8b71e227ac
SamarV-121 <samarvispute121@pm.me>
2022-09-17 14:45:43 +0100

    selene: Rename libwifi-hal.so * To prevent conflict with aosp's
    Change-Id: I922f26c45957f4bc5d2ad85e14ecfcb382abf811
    Signed-off-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>

--- device.mk
+++ device.mk
@@ -299,7 +299,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi.hostapd@1.2.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service-lazy
+    android.hardware.wifi@1.0-service-lazy.selene
 
 PRODUCT_PACKAGES += \
     libkeystore-wifi-hidl \
