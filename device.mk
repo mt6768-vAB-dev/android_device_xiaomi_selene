5d445004f269976449dab2651dfae01040978691
Arian <arian.kulmer@web.de>
2022-09-18 12:00:49 +0100

    selene: Provide a shim for libsink added opPackageName as a parameter to the constructor   of AudioTrack which breaks our libsink which was compiled   for an older libaudioclient.
    * fixes the following error:
    CANNOT LINK EXECUTABLE "/system/bin/vtservice": cannot locate symbol "_ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi" referenced by "/system/lib/libsink.so"
    
    https: //github.com/LineageOS/android_frameworks_av/commit/fd90fdfe2a611ae824a32e236da288b1fdfd445d
    Change-Id: I290e1beca02d46c4393d17356fa22741d771f847
    Signed-off-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>

--- device.mk
+++ device.mk
@@ -176,6 +176,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-base \
     mediatek-telephony-common
 
+PRODUCT_PACKAGES += \
+    libshim_vtservice
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
