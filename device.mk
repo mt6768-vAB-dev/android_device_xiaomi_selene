f3fd1814ee1ed738e421df90810c6a012d61cee7
TheMalachite <eliasgheeraert@gmail.com>
2022-10-08 13:10:52 +0300

    selene: Add persist.log.tag properties overrides
    Change-Id: Ibd6a89cbc0fa17b36fff1a0759afb64705687d13
    Signed-off-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>

--- device.mk
+++ device.mk
@@ -234,6 +234,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Properties
+include $(LOCAL_PATH)/vendor_logtag.mk
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
