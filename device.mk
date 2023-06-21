7e86ec513ead6db81c7152d5fd963caedac20e07
Michael Bestas <mkbestas@lineageos.org>
2023-06-21 11:20:22 +0200

    selene: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf
    Signed-off-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>

--- device.mk
+++ device.mk
@@ -234,6 +234,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.selene
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
