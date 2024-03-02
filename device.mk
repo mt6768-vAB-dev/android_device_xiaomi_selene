067e1c446935198e36f6c9558ff645c8b6bd059c
Mikazuuu07 <mikazuuu07@komodo-os.my.id>
2024-03-02 05:56:47 +0000

    selene: Totally Use Runtime Resource Overlay
    Signed-off-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>

--- device.mk
+++ device.mk
@@ -249,13 +249,19 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-lineage
 
+# RRO Overlays
 PRODUCT_PACKAGES += \
-    WifiOverlay \
-    TetheringConfigOverlay \
-    CarrierConfigOverlaySelene
+    SettingsProviderOverlaySelene \
+    CarrierConfigOverlay \
+    SystemUIOverlaySelene \
+    DialerOverlaySelene \
+    TelephonyOverlaySelene \
+    FrameworksResOverlaySelene \
+    TetheringResOverlaySelene \
+    SettingsOverlaySelene \
+    WifiResOverlaySelene
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
